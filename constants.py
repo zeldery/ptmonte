3426cf816bb3660f9ae1b740a8401eb61ee0f2a5
zeldery <zeldery@yahoo.com.vn>
2017-10-04 12:52:17 +0700

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,15 @@
+'''
+Monte Carlo code
+written by: Thien-Phuc Tu-Nguyen
+Last modified: 2017
+File: simulation
+'''
+
+# Temperature in K
+# Pressure in Pa
+# Energy in 1/kB K unit (standard energy divided by Boltzmann constant
+
+DE_BROGLIE = 17.458
+BOLTZMANN_ANGSTROM = 1.38065e7
+
+
